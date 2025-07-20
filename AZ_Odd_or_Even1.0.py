2330c9f22ec431835efa9f713af0cdcc2cc5ab1b
Volkath-MVP <aquilesvolkath71@gmail.com>
2025-07-20 18:28:01 -0300

    Português: estrutura inicial do jogo Janela — English: basic game structure Window

---
+++ AZ_Odd_or_Even1.0.py
@@ -0,0 +1,6 @@
+#All imported libraries
+import tkinter as tk
+root=tk.Tk() #Initialize window
+root.title("AZ Odd or Even")#Window title
+root.minsize(800, 600)#Minimum window size
+root.mainloop()#Keep window open
