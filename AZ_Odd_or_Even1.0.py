8e3c30e4c49b430cc2c47f492c519d9c7024555c
Volkath-MVP <aquilesvolkath71@gmail.com>
2025-07-21 13:19:22 -0300

    Português: Desenvolvimento da funcionalidade Impar ou Par - English: Development of the Odd or Even functionality

--- AZ_Odd_or_Even1.0.py
+++ AZ_Odd_or_Even1.0.py
@@ -12,6 +12,13 @@ Question.pack(pady=40) #This already works, so I’m not switching to .place :/
 #Result
 result=tk.Label(root, text="Where is your Motivation?", font=("Arial", 20))
 result.pack(pady=20) #This already works too, soooooo I’m not switching to .place :)
+#Odd or Even Function
+def check(AZ):
+    global number, response
+    response = (number % 2 == 0 and AZ=="Even") or (number % 2 != 0 and AZ == "Odd")
+    message = "YOU HAVE POWER!" if response else "You need more Energy..."
+    result.config(text = message, fg="Green" if response else "Red")
+    root.after(800, lambda: result.config(text=""))
 #Buttons
 #button Odd
 Button_Odd = tk.Button(root, text="Impar", font=("Arial", 12))
