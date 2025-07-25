7ba969e8a8c7929b78c599dd0dd228633f2ad1ae
Volkath-MVP <aquilesvolkath71@gmail.com>
2025-07-25 10:08:48 -0300

    Português: Função que adiciona e subtrai o score  -  English: Function that adds and subtracts the score.

--- AZ_Odd_or_Even1.0.py
+++ AZ_Odd_or_Even1.0.py
@@ -12,9 +12,9 @@ Question.pack(pady=40) #This already works, so I’m not switching to .place :/
 #Result
 result=tk.Label(root, text="Where is your Motivation?", font=("Arial", 20))
 result.pack(pady=20) #This already works too, soooooo I’m not switching to .place :)
-#Função para atualizar a pergunta APÓS a resposta
+#Function to update the question AFTER the answer
 def update_number():
-    global number, message, result
+    global number, message, result #Global calls the variables needed for the function, in this case "number", "message", and "result"
     number = ra.randint(1, 99)
     Question.config(text=f"O número {number} é Ímpar ou Par?")
     result.config(text="" )  #Reset message
@@ -23,12 +23,20 @@ def update_number():
 def reset_message():
     global message
     message=""
+#Function that adds and subtracts the score.
+def V(response):
+    global score
+    if response:
+        score += 500 #score addition
+    else:
+        score -=800 #score subtraction
 #Odd or Even Function
 def check(AZ):
     global number, response
     response = (number % 2 == 0 and AZ=="Even") or (number % 2 != 0 and AZ == "Odd")
     message = "YOU HAVE POWER!" if response else "You need more Energy..."
     result.config(text = message, fg="Green" if response else "Red")
+    V(response)
     root.after(800, lambda: result.config(text=""))
     root.after(800, lambda: reset_message())
     root.after(800, update_number)
