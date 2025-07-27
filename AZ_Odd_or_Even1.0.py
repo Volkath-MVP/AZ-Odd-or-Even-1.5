1310d0618256a283754443f2d2d2acb390b68f66
Volkath-MVP <aquilesvolkath71@gmail.com>
2025-07-27 15:40:20 -0300

    Português: Function to update the rank - English: rank functions included

--- AZ_Odd_or_Even1.0.py
+++ AZ_Odd_or_Even1.0.py
@@ -32,6 +32,7 @@ def check(AZ):#Yes, I added AZ in the code. I’m AZheaven, I need to leave my m
     message = "YOU HAVE POWER!" if response else "You need more Energy..."#initial message
     result.config(text = message, fg="Green" if response else "Red") #Message settings
     V(response)
+    atualizar_rank()
     root.after(800, lambda: result.config(text=""))
     root.after(800, lambda: reset_message())
     root.after(800, update_number)
@@ -46,6 +47,29 @@ def V(response): #Yeah, I put "V" because I didn’t know what to name it :/
     Score.config(text=f"{score}")  #Updates the score in the text. I hadn’t thought of that... Ah... It was the f-string... I had forgotten :/
 Score=tk.Label(root, text=f"{score}", font=("Arial", 12))
 Score.place(relx=0.90, rely=0.30, anchor="center") # ".place" refers to the general positioning of things
+#Function to update the rank
+rank_text= ""
+def atualizar_rank():
+    global score, rank_text
+    if score >= 7000:
+        rank_text = "SSS"
+    elif score >= 6000:
+        rank_text = "SS"
+    elif score >= 5000:
+        rank_text = "S"
+    elif score >= 4000:
+        rank_text = "A"
+    elif score>= 3000:
+        rank_text = "B"
+    elif score>= 2000:
+        rank_text = "C"
+    elif score>=1000:
+        rank_text = "D"
+    else:
+        rank_text = ""
+    rank_label.config(text=f"{rank_text}")
+rank_label=tk.Label(root, text=f"{rank_text}", font=("Arial", 12))
+rank_label.place(relx=0.90, rely=0.20, anchor="center")
 #Buttons
 #button Odd
 Button_Odd = tk.Button(root, text="Impar", font=("Arial", 12), command=lambda: check("Odd"))
